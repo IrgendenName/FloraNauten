8f89a341f1034d81b16092ca5c22476a22a3453a
Christian Spindler <christian.spindler@gmx.ch>
2023-11-07 13:47:39 +0100

    examples addresses with area size and type of area

---
+++ swisstopo/read_swisstopo.py
@@ -0,0 +1,8 @@
+import geopandas as gpd
+
+filepath = "C:/Users/cspin/Documents/AXA Hackathon/2023 Floranauten/HabitatMap_CH_V1_20220805.gdb"
+# filepath = "C:/Users/cspin/Downloads/lebensraumkarte-schweiz_2056.gdb.zip"
+
+# data = gpd.read_file(filepath)
+# type(data)
+
