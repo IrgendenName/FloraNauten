1c436f77a0dd2314421ac29c5ea94fcf0ff71426
wanlau01 <wanlau01@students.zhaw.ch>
2023-11-07 15:05:57 +0100

    backend update

--- backend/floranauten/src/main/java/ch/App.java
+++ backend/floranauten/src/main/java/ch/App.java
@@ -1,11 +1,10 @@
 package ch;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
