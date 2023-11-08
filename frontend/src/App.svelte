1ac9582ad462326a84645d126fef426c3710c8e2
C156744 <jamie.schoenauer@axa.ch>
2023-11-08 09:46:08 +0100

    add video

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -4,33 +4,23 @@
 </script>
 
 <!-- This part is always displayed -->
-
-<div class= "navigation container">
-	<nav class="navbar nav-fill w-100 navbar-expand-lg navbar-light bg-light">
-		<div class="container-fluid">
-			<div class="collapse navbar-collapse" id="navbarNavDropdown">
-				<ul class="navbar-nav">
-					<li class="nav-item">
-						<a class="nav-link" href="#/">Home</a>
-					</li>				
-					<li class="nav-item">
-						<a class="nav-link" href="#/rechner">Rechner</a>
-					</li>
-												
-				</ul>
-			</div>
-		</div>
-	</nav>
-
-</div>
-
-
 <div class="container mt-3">
-	<!-- Here we load the page depending on the current URL -->
+	<img class="img-fluid logo" alt="Logo" src="Logo.png" />
+
+
+	<!-- Here we load the page depending on the current 	URL -->
 	<Router {routes} />
 </div>
 
-
-
-
+<style>
+	.logo {
+		width: 5%;
+		height: auto;
+		margin-bottom: 100px;
+		float: right;
+		position: absolute;
+		top: 10px;
+		right: 10px;
+	}
+</style>
 
--- frontend/src/pages/Home.svelte
+++ frontend/src/pages/Home.svelte
@@ -1,51 +1,59 @@
 <script>
-   import { onMount } from "svelte";
-    import axios from "axios";
 </script>
 
 <main>
-  <img
-    src="https://www.zg.ch/behoerden/gemeinden/risch-rotkreuz/verwaltung/tiefbau-umwelt-sicherheit-1/copy_of_laerm/umwelt/biodiversitaet/ftw-simplelayout-textblock/@@images/fb1f43c6-f23e-4b67-a4ec-c5e0823d2677.jpeg"
-    alt="Biodiversität"
-  />
+  <video src="images/biodiversity.mp4" muted autoplay loop></video>
 
-  <h1>WARUM BIODIVERSITÄT FÜR UNS WICHTIG IST</h1>
-  <p>
-    Die Biodiversität – die Vielfalt der Gene, Arten und Lebensräume – ist
-    unsere Lebensgrundlage. In der Schweiz sind zurzeit die Hälfte der
-    Lebensräume und ein Drittel aller Arten bedroht. Darüber hinaus ist der
-    Erhalt der biologischen Vielfalt wichtig für Erfolge beim Klimaschutz. Wir
-    haben uns ambitionierte Ziele im Rahmen unserer Klimastrategie gesetzt: Als
-    gesellschaftliche Akteurin wollen wir menschlichen Fortschritt ermöglichen,
-    indem wir schützen, was wirklich wichtig ist – und dazu gehört die
-    Biodiversität.
-  </p>
-  <button class="cta-button"> VERBINDE DICH JETZT MIT DER NATUR </button>
+  <div class="overlay">
+    <h1>WARUM BIODIVERSITÄT FÜR UNS WICHTIG IST</h1>
+    <p>
+      Die Biodiversität – die Vielfalt der Gene, Arten und Lebensräume – ist
+      unsere Lebensgrundlage. In der Schweiz sind zurzeit die Hälfte der
+      Lebensräume und ein Drittel aller Arten bedroht. Darüber hinaus ist der
+      Erhalt der biologischen Vielfalt wichtig für Erfolge beim Klimaschutz. Wir
+      haben uns ambitionierte Ziele im Rahmen unserer Klimastrategie gesetzt: Als
+      gesellschaftliche Akteurin wollen wir menschlichen Fortschritt ermöglichen,
+      indem wir schützen, was wirklich wichtig ist – und dazu gehört die
+      Biodiversität.
+    </p>
+    <a class="nav-link" href="#/rechner">
+      <button class="cta-button">VERBINDE DICH JETZT MIT DER NATUR </button>
+    </a>
+  </div>
 </main>
 
 <style>
-  /* Hintergrundbild und Stil für die Seite */
-  main {
-    /* background-image: url("your-image-path.jpeg"); */
-    background-size: cover;
-    background-position: center;
+  main video{
+    width: 100vw;
     height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    text-align: center;
-    color: black;
+    object-fit: cover;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    z-index: -1;
+  }
+
+  .overlay{
+    position: fixed;
+    bottom: 5vh;
   }
 
   /* Stil für die Überschrift */
   h1 {
+    color: #fff;
     font-size: 3rem;
     margin-bottom: 2rem;
+    margin-top: 30vh;
+    font-family: 'Julius Sans One';
+
   }
 
   /* Stil für den Absatz */
   p {
-    font-size: 1.25rem;
+    color: #fff;
+    font-size: 1.0rem;
     margin-bottom: 2rem;
   }
 
