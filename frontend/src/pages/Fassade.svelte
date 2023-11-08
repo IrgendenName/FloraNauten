3c08a70d5c04a8fc69f37e059f8a3b64a8cd47c8
rietmjoe <rietmjoe@students.zhaw.ch>
2023-11-08 11:54:32 +0100

    Fassade

--- frontend/src/pages/Fassade.svelte
+++ frontend/src/pages/Fassade.svelte
@@ -2,8 +2,6 @@
   import { onMount } from "svelte";
   import axios from "axios";
 
-
-
   let fassade = {
     name: "Fassadenbegrünung",
     beschreibung: `Sie möchten Ihre Fassade begrünen. Das ist eine gute Idee. Die Begrünung Ihrer Fassade bringt vielfältige Vorteile für Mensch und Natur. Sie erhöhen das Wohlbefinden von uns Menschen und leisten einen wichtigen Beitrag zur Biodiversität. Ausserdem absorbieren die Begrünungen Stickoxide sowie Feinstaub und leisten somit einen wichtigen Beitrag für saubere Luft. Für die meisten Pflanzen ist die Installation einer Rankenhilfe notwendig.`,
@@ -12,7 +10,7 @@
     text11: "37 Stk. à CHF 50.-",
     text12: "(Drahtseile werden im Abstand von 40 cm an Fassade geschraubt)",
     kosten1: "CHF 1850",
-    text2: "Kletterpflanze Trompetenblume 37 Stk. à CHF 50.-",
+    text2: "Kletterpflanze Waldrebe 37 Stk. à CHF 50.-",
     kosten2: "CHF 1850",
     totaltext: "Total Materialkosten",
     totalpreis: "CHF 3700",
@@ -21,9 +19,9 @@
 
 <main>
   <h1>Empfohlene Massnahmen</h1>
-  <br> <br><br> <br>
+  <br /> <br /><br /> <br />
   <div class="massnahme">
-    
+    <img src="fassademassnahme.jpg" alt="Fassade" class="image" />
     <h3><b>{fassade.name}</b></h3>
     <br />
     <h3>Beschreibung</h3>
@@ -32,7 +30,10 @@
     <h3>Kosten</h3>
     <table>
       <tr>
-        <td>{fassade.text1}<br><br>{fassade.text11}<br><br>{fassade.text12}</td>
+        <td
+          >{fassade.text1}<br /><br />{fassade.text11}<br /><br
+          />{fassade.text12}</td
+        >
         <td>{fassade.kosten1}</td>
       </tr>
       <tr>
@@ -48,6 +49,14 @@
 </main>
 
 <style>
+  .image {
+    width: 100%;
+    max-width: 400px; /* Adjust the maximum width as needed */
+    height: auto;
+    display: block;
+    margin: 0 auto;
+  }
+
   main {
     background-size: cover;
     background-position: center;
@@ -85,8 +94,6 @@
   .beschreibung {
     margin-bottom: 10px;
   }
-
-
 
   table {
     width: 100%;
